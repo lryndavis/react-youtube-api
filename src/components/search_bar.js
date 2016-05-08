96d799c53b177a4365ce0ea85c41087fca9d3b5e
Lauryn Davis <lryndavis@gmail.com>
2016-05-08 14:45:23 -0700

    add search input event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
+//each instance of a class based component has its own copy of state
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+//initialize state by creating a new object
+//term is where the change is recorded
+    this.state = {term: ''};
+    //inside constructor is the ONLY time you initalize state
+    //setState should be used EVERYWHERE else
+  }
+
   render() {
     //event handler triggers whenever event occurs
     //onChange is a react defined prop
+    //define method for event handler, called with event object
+    //refactored to be included in onChange
     //pass event handler to element to be watched for event
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+    //whenever value changes, this runs and new value of input is rendered into state
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+      </div>
+    );
   }
 }
-
-//event handler
-//define method for event handler, called with event object
-//refactored to be included in onChange
-
 
 
 export default SearchBar;
